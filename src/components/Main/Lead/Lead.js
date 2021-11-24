7ec0600118534b44101ee677f48bb593d83e639f
Sophie Milash <sophie.milash@yandex.ru>
2021-11-24 12:37:57 +0200

    style: add hover effect to lead links

--- src/components/Main/Lead/Lead.js
+++ src/components/Main/Lead/Lead.js
@@ -7,10 +7,26 @@ function Lead() {
       <h1 className="lead__title">Frontend development</h1>
       <ul className="lead__list">
         <li className="lead__list-item">
-          <AnchorLink href="#about" className="app__link lead__link">/About me</AnchorLink>
+          <AnchorLink href="#about" className="app__link lead__link">
+            <span className="lead__link-option">a#425t @m</span>
+            <span className="lead__link-option">A1$8b! j&</span>
+            <span className="lead__link-option">\A@64t $$</span>
+            <span className="lead__link-option">/A5d0? t#</span>
+            <span className="lead__link-option">Ab0$it 9e</span>
+            <span className="lead__link-option">/Abou! ne</span>
+            <span className="lead__link-final" >/About me</span>
+          </AnchorLink>
         </li>
         <li className="lead__list-item">
-          <AnchorLink href="#skills" className="app__link lead__link">/Skill set</AnchorLink>
+          <AnchorLink href="#skills" className="app__link lead__link">
+            <span className="lead__link-option">s&9Lir $rt</span>
+            <span className="lead__link-option">8ki\ep 5#2</span>
+            <span className="lead__link-option">$p8b!; $$t</span>
+            <span className="lead__link-option">/Sm9d? j#y</span>
+            <span className="lead__link-option">Sk0il5 9el</span>
+            <span className="lead__link-option">/Ski!! $e?</span>
+            <span className="lead__link-final" >/Skill set</span>
+          </AnchorLink>
         </li>
       </ul>
 
