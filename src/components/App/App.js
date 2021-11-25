047bff0854509987d86617a14622c848d87e28dd
Sophie Milash <sophie.milash@yandex.ru>
2021-11-25 16:54:27 +0200

    fix: fix basename for BrowserRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,40 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Contact from '../Contact/Contact';
 import Main from '../Main/Main';
 import Projects from '../Projects/Projects';
 import Sidebar from '../Sidebar/Sidebar';
 
 function App() {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    console.log(process.env.PUBLIC_URL);
+    console.log(window.location.href);
+    console.log(location);
+  }, [location]);
+
   return (
     <>
       <main>
-        <Routes>
+        <Routes basename={process.env.PUBLIC_URL + '/'} >
           <Route path="/"
             element={
               <Main />
             }
-            />
+          />
 
           <Route path="/projects"
             element={
               <Projects />
             }
-            />
+          />
 
           <Route path="/contact"
             element={
               <Contact />
             }
-            />
+          />
         </Routes>
 
         <Sidebar />
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
