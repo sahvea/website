415824ea60cf296f4c69801a63af0ad619b7204e
Sophie Milash <sophie.milash@yandex.ru>
2021-11-26 16:54:04 +0200

    feat: add WakaTime statistics

--- src/components/Main/Skills/Skills.js
+++ src/components/Main/Skills/Skills.js
@@ -3,6 +3,13 @@ function Skills() {
     <section className="skills app__section" id="skills">
       <h2 className="app__section-title">/Skill set</h2>
 
+      <div className="app__section-content">
+
+        <figure className="skills__statistics">
+          <embed src="https://wakatime.com/share/@sahvea/6ef395c9-1def-452f-92b8-ced06f973189.svg"></embed>
+        </figure>
+      </div>
+
     </section>
   );
 }
