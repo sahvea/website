497a9dbf8b19cc2b2cd0366ed7448de4438b67c7
Sophie <sophie.milash@yandex.ru>
2021-11-27 20:23:45 +0200

    feat: add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Contact from '../Contact/Contact';
+import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
 import Projects from '../Projects/Projects';
 import Sidebar from '../Sidebar/Sidebar';
@@ -32,7 +33,7 @@ function App() {
 
         <Sidebar />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
