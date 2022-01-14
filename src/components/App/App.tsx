dd3df73c65a2df0537474481c00c51a06a724c42
Sophie Milash <sophie.milash@yandex.ru>
2022-01-14 16:40:27 +0200

    feat: add preloading

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { TranslationContext } from '../../contexts/translationContext';
 import { translations } from '../../utils/translations';
 import Contact from '../Contact/Contact';
+import Flying from '../Deco/Flying/Flying';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
@@ -12,6 +13,7 @@ import Sidebar from '../Sidebar/Sidebar';
 
 const App: React.FC = () => {
   const [lang, setLang] = useState<string>('en');
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [mobResolution, setMobResolution] = useState<boolean>(false);
   const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
 
@@ -35,28 +37,47 @@ const App: React.FC = () => {
     document.documentElement.lang = lang;
   }, [lang]);
 
+  useEffect(() => {
+    document.onreadystatechange = function() {
+      if (document.readyState === 'loading') {
+        setIsLoading(true);
+      } else if (document.readyState === 'interactive') {
+        setIsLoading(true);
+      } else if (document.readyState === 'complete') {
+        setIsLoading(false);
+      }
+   }
+  }, [isLoading]);
 
   return (
     <TranslationContext.Provider value={translations[lang]}>
 
-      { mobResolution && <Header onLangClick={setLang} /> }
+      {isLoading
+        ?
+        <div className='app__loading'>
+          <Flying />
+        </div>
+        :
+        <>
+          { mobResolution && <Header onLangClick={setLang} /> }
 
-      <main>
-        <Routes>
-          <Route path="/" element={ <Main /> } />
+          <main>
+            <Routes>
+              <Route path="/" element={ <Main /> } />
 
-          <Route path="/projects" element={ <Projects /> } />
+              <Route path="/projects" element={ <Projects /> } />
 
-          <Route path="/contact" element={ <Contact /> } />
+              <Route path="/contact" element={ <Contact /> } />
 
-          <Route path="*" element={ <NotFound /> } />
-        </Routes>
+              <Route path="*" element={ <NotFound /> } />
+            </Routes>
 
-        { !mobResolution && <Sidebar onLangClick={setLang} /> }
-      </main>
-      <Footer />
+            { !mobResolution && <Sidebar onLangClick={setLang} /> }
+          </main>
+          <Footer />
+        </>
+      }
     </TranslationContext.Provider>
   );
 }
-
 export default App;
