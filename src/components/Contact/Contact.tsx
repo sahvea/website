158f795ed0339a4b46b68770f2633601f5299a78
Sophie <sophie.milash@yandex.ru>
2022-02-21 02:37:30 +0200

    feat: add waiting gifs

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,15 @@
-const Contact: React.FC = () =>{
+const Contact: React.FC = () => {
   return (
     <section className="app__section">
       <h1>Contact information</h1>
+
+      <img
+        src="https://c.tenor.com/-P-xeHYEY9QAAAAd/sad-pablo-lonely.gif"
+        alt="waiting"
+        style={{ marginTop: "50px", maxWidth: "80vw" }}
+      />
     </section>
   );
-}
+};
 
 export default Contact;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,8 +2,14 @@ const Projects: React.FC = () => {
   return (
     <section className="app__section">
       <h1>Projects will be here</h1>
+
+      <img
+        src="https://c.tenor.com/-P-xeHYEY9QAAAAd/sad-pablo-lonely.gif"
+        alt="waiting"
+        style={{ marginTop: "50px", maxWidth: "80vw" }}
+      />
     </section>
   );
-}
+};
 
 export default Projects;
